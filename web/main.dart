3c02f0afe92da1d45a42de9745b647e1fa902026
Shahryar Pirooz <dev.shahryar@gmail.com>
2020-12-10 16:01:04 +0330

    fix bug name

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,24 @@
 import 'dart:html';
 
-Element nav, mainCardLink;
+Element nav, mainCardLink, darkMode;
+bool isDark = false;
 
 void main() {
+  //elements
   nav = querySelector('#my-nav')
     ..hidden = true
-    ..style.opacity = '0%';
-  mainCardLink = querySelector('#main-card-link');
+    ..style.opacity = '0%'; //nav cast
+
+  mainCardLink = querySelector('#main-card-link'); //cast a link in main card
+
+  darkMode = querySelector('#dark-mode');
+
+  darkMode.onClick.listen((event) {
+    isDark = isDark == false ? true : false;
+    dark(isDark);
+  });
+
+  // change navbar's alpha
   hiddenNav();
 }
 
@@ -20,4 +32,12 @@ void hiddenNav() {
       nav.hidden = true;
     }
   });
+}
+
+void dark(isDark) {
+  // var stile = document.head.querySelector('link[href="stile.css"]');
+  var style = document.head.querySelector('#style');
+  style.attributes['href'] =
+      isDark ? 'css/style-dark-mode.min.css' : 'css/style.min.css';
+  print(style.toString());
 }
